edd11d5353b82b42659cee3445b515faa7b952b4
João Victor <joao.adm.etec@outlook.com>
2024-01-03 23:50:10 -0300

    Adicionar arquivos de projeto.

---
+++ UsuarioApi/Controllers/UsuarioController.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UsuarioApi.Data.Dtos;
+using UsuarioApi.Models;
+using UsuarioApi.Services;
+
+namespace UsuarioApi.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        private CadastroService _cadastroService;
+
+        public UsuarioController(CadastroService cadastroService)
+        {
+            _cadastroService = cadastroService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CadastraUsuario(CreateUsuarioDto dto)
+        {
+            await _cadastroService.CadastraAsync(dto);
+            return Ok("Usuário cadastrado!");
+        }
+    }
+}
